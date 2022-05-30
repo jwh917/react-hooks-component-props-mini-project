4cb55db2870e1d128ff4c8faaa8aa0ab31ac5d2f
Julien Hnery <jwh_917@yahoo.com>
2022-05-29 22:15:54 -0400

    test data

--- src/data/blog.js
+++ src/data/blog.js
@@ -10,7 +10,7 @@ const blogData = {
       title: "Components 101",
       date: "December 15, 2020",
       preview: "Setting up the building blocks of your site",
-      minutes: 5,
+      minutes: 4,
     },
     {
       id: 2,
@@ -19,13 +19,41 @@ const blogData = {
       preview: "Passing props is never passé",
       minutes: 15,
     },
-    {
+    { 
       id: 3,
+      title: "React 🖤",
+      date: "September 17, 2020",
+      preview: "EveryOne Loves React",
+      minutes: 22,
+    },
+    {
+      id: 4,
+      title: "Java0",
+      date: "August 4, 2020",
+      preview: "Cup Of Java",
+      minutes: 30,
+    },  
+    {
+      id: 4,
+      title: "Python Made Easy",
+      date: "April 3, 2020",
+      preview: "Python101",
+      minutes: 45,
+    },   
+    {
+      id: 5,
+      title: "Rails/Ruby 🚊💎",
+      date: "June 15, 2020",
+      preview: "Knowledge is POWER",
+      minutes:57,
+    },   
+    {
+      id: 6,
       title: "Function vs Class Components",
       preview: "React, meet OOJS.",
-      minutes: 47,
-    },
-  ],
+      minutes: 63,
+    }
+  ]
 };
 
 export default blogData;
